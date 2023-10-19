06f78156fb1ec5135701abfadb46775d5297d759
Ines Chung-Halpern <ines.chung-halpern.23@ucl.ac.uk>
2023-10-19 15:31:05 +0100

    adding jupyter notebooks

---
+++ lecture_3/scatter_nb_export.py
@@ -0,0 +1,21 @@
+# %%
+import numpy as np
+import matplotlib.pyplot as plt
+
+# %%
+N = 50
+x = np.random.rand(N)
+y = np.random.rand(N)
+
+# %%
+colours = np.random.rand(N)
+area = (30*np.random.rand(N))**2
+
+# %%
+plt.scatter(x,y,s= area, c = colours, alpha = 0.5)
+plt.show()
+
+# %%
+
+
+
